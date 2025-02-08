8a869655823896b31bafa758dc7eba7c4c3fc264
Jan <110758056+leafboo@users.noreply.github.com>
2025-02-08 11:21:38 +0800

    Download the login component

---
+++ src/accounts.ts
@@ -0,0 +1,16 @@
+export const userAccounts = [
+  {
+    email: "python@gmail.com",
+    password: "1to4"
+  }, {
+    email: "javascript@gmail.com",
+    password: "1234"
+  }, {
+    email: "godot@gmail.com",
+    password: "5to9"
+  }, {
+    email: "ruby@gmail.com",
+    password: "56789"
+  },
+
+]
--- src/pages/authentication/Login.tsx
+++ src/pages/authentication/Login.tsx
@@ -1,32 +1,15 @@
-import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card"
-import { Label } from "@radix-ui/react-label"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
+import { LoginForm } from "@/components/login-form"
+import { userAccounts } from "@/accounts"
+
 
 export default function Login() {
   return (
     <>
-      <Card className="mx-auto max-w-sm">
-        <CardHeader className="space-y-1">
-          <CardTitle className="text-2xl font-bold">Login</CardTitle>
-          <CardDescription>Enter your email and password to login to your account</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="space-y-4">
-            <div className="space-y-2">
-              <Label htmlFor="email">Email</Label>
-              <Input id="email" type="email" placeholder="m@example.com" required />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" required />
-            </div>
-            <Button type="submit" className="w-full">
-              Login
-            </Button>
-          </div>
-        </CardContent>
-      </Card>
+      <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+        <div className="w-full max-w-sm">
+          <LoginForm />
+        </div>
+      </div>
     </>
   )
 }
